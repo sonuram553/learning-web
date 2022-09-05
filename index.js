27cd8819b3bdbe93c32b0d2a688d5a1621ef774f
Sonu Ram <sonuram6960@gmail.com>
2022-09-05 23:16:03 +0530

    Static members of a Class

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-// Unnamed
-let Rectangle = class {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
-};
 
-console.log(Rectangle.name); // Rectangle
+  static displayName = "Point";
+  static distance(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
 
-// Named
-Rectangle = class Rectangle2 {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
+    return Math.hypot(dx, dy);
   }
 }
 
-console.log(Rectangle.name); // Rectangle2
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+
+console.log(Point.displayName); // Point;
+console.log(Point.distance(p1, p2));
