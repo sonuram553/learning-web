b73eec400985adce1b26b0a2f3c762d3111eb0b9
Sonu Ram <sonuram6960@gmail.com>
2022-09-05 23:00:22 +0530

    Class expressions

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-function strict1(str) {
-  "use strict";
-  return eval(str);
+// Unnamed
+let Rectangle = class {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+};
+
+console.log(Rectangle.name); // Rectangle
+
+// Named
+Rectangle = class Rectangle2 {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
 }
 
-function strict2(f, str) {
-  "use strict";
-  return f(str);
-}
-
-function nonStrict(str) {
-  return eval(str);
-}
-
-strict1("'Strict mode code!'");
-strict1("'use strict'; 'Strict mode code!'");
-
-strict2(eval, "'Non-strict code.'");
-strict2(eval, "'use strict'; 'Strict mode code!'");
-
-nonStrict("'Non-strict code.'");
-nonStrict("'use strict'; 'Strict mode code!'");
+console.log(Rectangle.name); // Rectangle2
